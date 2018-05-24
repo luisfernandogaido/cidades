96397c07e3a4b1db435e30e0bc34dba0dba145d1
Luís Fernando Gaido <luisfernandogaido@gmail.com>
2018-05-24 18:24:42 -0300

    senha borrada

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-file=/var/www/html/municipios.gaido.net.br/deploy/service
+file=/var/www/html/municipios.gaido.net.br/deploy/cidades
 if [ -e "$file" ]; then
     systemctl stop municipios.service
     mv "$file" /var/www/html/municipios.gaido.net.br
-    chmod 0774 /var/www/html/municipios.gaido.net.br/service
+    chmod 0774 /var/www/html/municipios.gaido.net.br/cidades
     systemctl start municipios.service
 fi
